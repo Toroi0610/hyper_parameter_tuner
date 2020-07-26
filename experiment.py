3abb4c8f80ef2b3bd045d7c3587ed2ba1458165a
Toroi0610 <toroi0610@gmail.com>
2020-07-26 11:00:18 +0900

    ok

--- experiment.py
+++ experiment.py
@@ -9,12 +9,8 @@ from hyperparameter_optimizer import create_objective
 # Model
 from model.lightgbm_0.model import learning
 
-# load api_token (Please register https://ui.neptune.ai/auth/realms/neptune/protocol/openid-connect/registrations?client_id=neptune-frontend&redirect_uri=https%3A%2F%2Fui.neptune.ai%2Fafter-registration&state=db8b7051-097e-44e5-83cc-ed156f15e995&response_mode=fragment&response_type=code&scope=openid&nonce=f2f5767b-e16b-47b9-8944-4fdff4d39f80)
-with open("api_token.txt", "r") as f:
-    api_token = f.read()
 # set UserName/ExperimentName
-neptune.init('User/House_Price',
-             api_token=api_token)
+neptune.init('toroi/HousePrice')
 
 # set Train/Test file path
 TRAIN_FILEPATH = '/data/train.csv'
@@ -27,7 +23,7 @@ with neptune.create_experiment():
 training_data, training_target = data_loader(TRAIN_FILEPATH, TARGET_LIST)
 test_data, _ = data_loader(TEST_FILEPATH)
 
-neptune.create_experiment('House_Price')
+neptune.create_experiment('HousePrice')
 neptune_callback = optuna_utils.NeptuneCallback()
 
 objective = lambda trial : create_objective(trial, learning, training_data, training_target, metric, validation_size=0.25)
